16cbfb67bffac932f7f5842db6dd42e9104376e3
JungHanYoung <8735132@naver.com>
2021-04-27 02:47:32 +0900

    문제 풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,36 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String[] solution(String[] strList) {
+        StringBuilder sb;
+        String[] reversedStrList = new String[strList.length];
+
+        for(int i = 0; i < strList.length; i++) {
+            sb = new StringBuilder();
+            String targetStr = strList[i];
+            for (int j = targetStr.length() - 1; j >= 0; j--) {
+                sb.append(targetStr.charAt(j));
+            }
+            reversedStrList[i] = sb.toString();
+        }
+
+        return reversedStrList;
+    }
+
     public static void main(String[] args) {
+
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int inputCount = in.nextInt();
+        String[] inputStrList = new String[inputCount];
+        for (int i = 0; i < inputCount; i++) {
+            inputStrList[i] = in.next();
+        }
+//        System.out.println("inputStr : " + inputStr);
+//        System.out.println("inputChar : " + inputChar);
+        String[] result = solution(inputStrList);
+        for (String s :
+                result) {
+            System.out.println(s);
+        }
     }
 }
