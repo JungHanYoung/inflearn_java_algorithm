b8f513003e9bab49c2c94befbce1723326aecd3b
JungHanYoung <8735132@naver.com>
2021-04-27 03:36:10 +0900

    강의 참조, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String apws = s + ' ';
+        int cnt = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if(apws.charAt(i) == apws.charAt(i+1)) {
+                cnt++;
+            } else {
+                sb.append(apws.charAt(i));
+                if(cnt != 1) {
+                    sb.append(cnt);
+                    cnt = 1;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String input = in.next();
+        System.out.println(solution(input));
     }
 }
