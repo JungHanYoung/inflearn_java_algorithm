71a9db14bd3d1883e116f52d53ae6473e078381f
JungHanYoung <8735132@naver.com>
2021-04-27 02:45:23 +0900

    문제 풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isLowerCase(s.charAt(i))) {
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            } else {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String inputStr = in.next();
+        //        System.out.println("inputStr : " + inputStr);
+        //        System.out.println("inputChar : " + inputChar);
+        String result = solution(inputStr);
+        System.out.println(result);
     }
 }
