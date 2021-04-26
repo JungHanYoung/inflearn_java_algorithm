4bfe6f6c1403730f39f963aa89591e31b2b7ff80
JungHanYoung <8735132@naver.com>
2021-04-27 02:48:29 +0900

    강의 참조, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String solution(String s) {
+        char[] chars = s.toCharArray();
+        int lt=0, rt=s.length()-1;
+
+        while(lt<rt) {
+            if(!Character.isAlphabetic(chars[lt])) lt++;
+            else if(!Character.isAlphabetic(chars[rt])) rt--;
+            else {
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+
     public static void main(String[] args) {
+
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String inputStr = in.next();
+        String solution = solution(inputStr);
+        System.out.println(solution);
+//        char inputChar = in.next().charAt(0);
+//        System.out.println("inputStr : " + inputStr);
+//        System.out.println("inputChar : " + inputChar);
     }
 }
