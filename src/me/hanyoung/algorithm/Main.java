73900db6c2d5079a1df47f3cbecfb9abe811726e
JungHanYoung <8735132@naver.com>
2021-04-27 02:50:18 +0900

    문제 풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static boolean solution(String s) {
+        char[] chars = s.toUpperCase().toCharArray();
+        int lt=0, rt=s.length()-1;
+
+        while(lt<rt) {
+            if(chars[lt] != chars[rt]) return false;
+            lt++;
+            rt--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String inputStr = in.next();
+        if(solution(inputStr)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
