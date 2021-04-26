95b690ea158fe46df576d1700d92326f209e7df3
JungHanYoung <8735132@naver.com>
2021-04-27 02:52:09 +0900

    강의 참조, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static boolean solution(String s) {
+        String str = s.toUpperCase().replaceAll("[^A-Z]", "");
+        return new StringBuilder(str)
+                .reverse()
+                .toString()
+                .equals(str);
+    }
+
     public static void main(String[] args) {
+
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String inputStr = in.nextLine();
+        if(solution(inputStr)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
