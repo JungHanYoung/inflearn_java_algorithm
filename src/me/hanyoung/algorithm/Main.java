eb6cad00157b6043ed2b0a7499b9d305df36d4a2
JungHanYoung <8735132@naver.com>
2021-04-27 03:45:58 +0900

    문제풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,23 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String solution(int num, String str) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < str.length(); i += 7) {
+            String secretAlphabet = str.substring(i, i+7);
+            secretAlphabet = secretAlphabet.replaceAll("[#]", "1");
+            secretAlphabet = secretAlphabet.replaceAll("[*]", "0");
+            int parseInt = Integer.parseInt(secretAlphabet, 2);
+            answer.append((char) parseInt);
+        }
+
+        return answer.toString();
+    }
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String input2 = in.next();
+        System.out.println(solution(input1, input2));
     }
 }
