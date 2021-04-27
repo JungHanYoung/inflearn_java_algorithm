15bc0c869f6c4201bae4acd744e38d1c2289769f
JungHanYoung <8735132@naver.com>
2021-04-27 12:42:57 +0900

    문제풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int solution(int[] list) {
+        int target = 0;
+        int sum = 0;
+        for (int i : list) {
+            if(target < i) {
+                sum++;
+                target = i;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int[] list = new int[in.nextInt()];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = in.nextInt();
+        }
+        System.out.println(solution(list));
     }
 }
