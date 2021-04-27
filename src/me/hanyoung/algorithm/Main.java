bdc895ea96252d5d0d55f89ba8bbe9466e253b86
JungHanYoung <8735132@naver.com>
2021-04-27 12:56:29 +0900

    문제풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int[] solution(int count) {
+        int[] piboList = new int[count];
+
+        piboList[0] = piboList[1] = 1;
+        for (int i = 2; i < count; i++) {
+            piboList[i] = piboList[i-2] + piboList[i-1];
+        }
+
+        return piboList;
+    }
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int[] result = solution(in.nextInt());
+        for (int j : result) {
+            System.out.print(j + " ");
+        }
     }
 }
