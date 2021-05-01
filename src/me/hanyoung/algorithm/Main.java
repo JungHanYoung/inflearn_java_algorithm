006436c6859a014ce8f0e6e1787ce569e24e22ef
JungHanYoung <8735132@naver.com>
2021-05-02 00:06:33 +0900

    강의 지참, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,37 @@
 package me.hanyoung.algorithm;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int[] result = solution(n, arr);
+        for (int j : result) {
+            System.out.print(j + " ");
+        }
     }
+
+    private static int[] solution(int n, int[] arr) {
+        int[] answer = new int[arr.length];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = 1;
+            for (int j = 0; j < n; j++) {
+                if(i == j) continue;
+                if(arr[i] < arr[j]) answer[i]++;
+            }
+        }
+
+        return answer;
+    }
+
+
 }
