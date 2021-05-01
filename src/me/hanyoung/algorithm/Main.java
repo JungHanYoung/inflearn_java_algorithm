707db224a488029af741d4d82dabd8866ac2f971
JungHanYoung <8735132@naver.com>
2021-05-01 21:32:51 +0900

    풀이하였으나 사이트 채점 통과X

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,58 @@
 package me.hanyoung.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    public static int getMax(int[] arrInt) {
+        int maxValue = Integer.MIN_VALUE;
+        for (int j : arrInt) {
+            if (maxValue < j) {
+                maxValue = j;
+            }
+        }
+        return maxValue;
+    }
+
+    public static List<Integer> solution(int[] arrInt) {
+        List<Integer> ret = new ArrayList<>();
+        int maxInArr = getMax(arrInt);
+        boolean[] primeArr = new boolean[maxInArr+1];
+        for (int i = 2; i <= maxInArr; i++) {
+            primeArr[i] = true;
+        }
+        for (int i = 2; i <= maxInArr; i++) {
+            if(primeArr[i]) {
+                for (int j = i * i; j <= maxInArr; j += i) {
+                    primeArr[j] = false;
+                }
+            }
+        }
+
+        for (int value :
+                arrInt) {
+            if (value < 2) continue;
+            if(primeArr[value]) ret.add(value);
+        }
+
+        return ret;
+    }
+
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int[] intList = new int[count];
+        for (int i = 0; i < count; i++) {
+            intList[i] = Integer.parseInt(new StringBuilder(sc.next()).reverse()
+                    .toString());
+        }
+        List<Integer> result = solution(intList);
+        for (int value :
+                result) {
+            System.out.print(value + " ");
+        }
+
     }
 }
