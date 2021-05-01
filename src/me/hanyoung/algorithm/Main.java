13a981259ccfbcec9e3dc7cfafaed92f2d3bbdfa
JungHanYoung <8735132@naver.com>
2021-05-01 23:50:04 +0900

    풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int solution(int n, int[] arr) {
+        int answer = 0;
+
+        int add = 1;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == 1) {
+                answer += add;
+                add++;
+            } else {
+                add = 1;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int result = solution(n, arr);
+        System.out.println(result);
+
     }
 }
