c66e00fc6968795f0a747552068e9daab2e79379
JungHanYoung <8735132@naver.com>
2021-05-05 03:25:22 +0900

    풀이 및 채점 확인

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int length = in.nextInt();
+        int[][] matrix = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = in.nextInt();
+            }
+        }
+        int result = solution(length, matrix);
+        System.out.println(result);
+    }
+
+    private static int solution(int length, int[][] matrix) {
+        int answer = 0;
+        int[][] map = new int[length+2][length+2];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                map[i+1][j+1] = matrix[i][j];
+            }
+        }
+
+        for (int i = 1; i <= length; i++) {
+            for (int j = 1; j <= length; j++) {
+                if(map[i][j] > map[i-1][j] &&
+                map[i][j] > map[i][j-1] &&
+                map[i][j] > map[i+1][j] &&
+                map[i][j] > map[i][j+1]) answer++;
+            }
+        }
+
+        return answer;
     }
 }
