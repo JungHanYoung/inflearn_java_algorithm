495446f8a83b7c5c638bdf93f2e066699ef7d98e
JungHanYoung <8735132@naver.com>
2021-05-08 15:19:54 +0900

    강의 참조, 재풀이 필요

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int[][] arr = new int[n+1][6];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int result = solution(n, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int n, int[][] arr) {
+        int answer = 0;
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if(arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt > maxValue) {
+                maxValue = cnt;
+                answer = i;
+            }
+        }
+
+        return answer;
     }
 }
