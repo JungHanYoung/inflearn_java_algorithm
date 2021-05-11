04a125cd5b1ee2398e393da61134254d7eb5b02d
JungHanYoung <8735132@naver.com>
2021-05-11 12:07:41 +0900

    풀이 하였으나 채점실패

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,34 @@
 package me.hanyoung.algorithm;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int result = solution(n, m, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int n, int m, int[] arr) {
+        int pi = 0;
+        int answer = Integer.MIN_VALUE;
+        while(pi + m < arr.length) {
+            int sum = 0;
+            for (int i = pi; i < pi + m; i++) {
+                sum += arr[i];
+            }
+            if(answer < sum) answer = sum;
+            pi++;
+        }
+
+        return answer;
     }
 }
