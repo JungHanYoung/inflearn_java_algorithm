17fecf11619e1b23d01d23551a4cbd7c6ff55749
JungHanYoung <8735132@naver.com>
2021-05-13 01:28:14 +0900

    재풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String str = in.nextLine();
+        String result = solution(str);
+        System.out.println(result);
+    }
+
+    private static String solution(String str) {
+        char[] chars = str.toCharArray();
+        int lt = 0, rt = chars.length - 1;
+        while(lt < rt) {
+            if(!Character.isAlphabetic(chars[lt])) lt++;
+            else if(!Character.isAlphabetic(chars[rt])) rt--;
+            else if(Character.toLowerCase(chars[lt++]) != Character.toLowerCase(chars[rt--])) {
+                return "NO";
+            }
+        }
+        return "YES";
     }
 }
