87d172768eccfba4ec3d4f838e4683178ff690a8
JungHanYoung <8735132@naver.com>
2021-05-12 22:24:27 +0900

    강의 지참함, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int len = in.nextInt();
+        int n = in.nextInt();
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = in.nextInt();
+        }
+        int result = solution(len, n, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int len, int n, int[] arr) {
+        int answer = Integer.MIN_VALUE;
+        int cnt = 0;
+        int lt = 0;
+        for (int rt = 0; rt < len; rt++) {
+            if(arr[rt] == 0) {
+                cnt++;
+                while(cnt > n) {
+                    if(arr[lt] == 0) {
+                        cnt--;
+                    }
+                    lt++;
+                }
+            }
+            if(answer < rt - lt + 1) answer = rt - lt + 1;
+        }
+        return answer;
     }
 }
