7e98f03ec7983a6fcad39cd8a8aa03b5a00863dc
JungHanYoung <8735132@naver.com>
2021-05-12 20:48:02 +0900

    풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int result = solution(n);
+        System.out.println(result);
+    }
+
+    private static int solution(int n) {
+        int lt = 1;
+        int sum = 0;
+        int cnt = 0;
+        for (int rt = 1; rt <= n / 2 + 1; rt++) {
+            sum += rt;
+            if(sum > n) {
+                while(sum >= n) {
+                    if(sum == n) cnt++;
+                    sum -= lt++;
+                }
+            } else if(sum == n) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
