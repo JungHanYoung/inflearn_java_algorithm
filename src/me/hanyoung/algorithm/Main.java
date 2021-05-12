1791c6d818a68fe50886ca7041499930718c28df
JungHanYoung <8735132@naver.com>
2021-05-13 01:55:52 +0900

    재풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String str = in.next();
+        String result = solution(str);
+        System.out.println(result);
+    }
+
+    private static String solution(String str) {
+        StringBuilder builder = new StringBuilder();
+        char[] chars = new char[str.length()+1];
+        chars[str.length()] = ' ';
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = str.charAt(i);
+        }
+        int cnt = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if(chars[i] == chars[i+1]) cnt++;
+            else {
+                builder.append(chars[i]);
+                if(cnt > 1) {
+                    builder.append(cnt);
+                }
+                cnt = 1;
+            }
+        }
+        return builder.toString();
     }
 }
