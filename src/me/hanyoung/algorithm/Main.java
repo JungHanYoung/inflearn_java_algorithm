ac9157ca1d3e3f5b4443b0366cd011fb8ebd066b
JungHanYoung <8735132@naver.com>
2021-05-15 00:46:45 +0900

    재풀이 및 채점완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int result = solution(n, m, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int l = 0; l < n; l++) {
+                        if(arr[k][l] == i) pi = l;
+                        if(arr[k][l] == j) pj = l;
+                    }
+                    if(pi < pj) cnt++;
+                }
+                if(cnt == m) answer++;
+            }
+        }
+        return answer;
     }
 }
