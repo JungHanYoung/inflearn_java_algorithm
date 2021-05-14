9254005aa65761dbcf5c2e47878d9c3ccd7f780b
JungHanYoung <8735132@naver.com>
2021-05-15 01:08:23 +0900

    풀이 및 채점완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,35 @@
 package me.hanyoung.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int num = in.nextInt();
+        String n = in.next();
+        char result = solution(num, n);
+        System.out.println(result);
+    }
+
+    private static char solution(int num, String n) {
+        char[] chars = n.toCharArray();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < num; i++) {
+            hashMap.put(chars[i], hashMap.getOrDefault(chars[i], 0) + 1);
+        }
+        char answer = ' ';
+        int max = Integer.MIN_VALUE;
+        for (Map.Entry<Character, Integer> entry:
+        hashMap.entrySet()){
+            if(max < entry.getValue()) {
+                max = entry.getValue();
+                answer = entry.getKey();
+            }
+        }
+
+        return answer;
     }
 }
