4792c6f6772c3f4cdf94797e04bced7971c66b8f
JungHanYoung <8735132@naver.com>
2021-05-14 21:32:35 +0900

    재풀이 및 채점완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int num = in.nextInt();
+        int[][] arr = new int[num+1][6];
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int result = solution(num, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int num, int[][] arr) {
+        int answer=0, max=Integer.MIN_VALUE;
+        for (int i = 1; i <= num; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= num; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if(arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt > max) {
+                max = cnt;
+                answer = i;
+            }
+        }
+        return answer;
     }
 }
