ed761f7c8bdf296b86098e436647cf254f423c83
JungHanYoung <8735132@naver.com>
2021-05-16 01:17:44 +0900

    풀이 완료 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,41 @@
 package me.hanyoung.algorithm;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String str = in.next();
+
+        solution(str);
+    }
+
+    private static void solution(String str) {
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c :
+                chars) {
+            if(c == '(') {
+                stack.push(c);
+            }
+
+            if(c == ')') {
+                if(stack.empty()) {
+                    System.out.println("NO");
+                    return;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if(!stack.isEmpty()) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
     }
 }
