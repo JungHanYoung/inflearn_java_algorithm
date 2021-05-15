5ac547b8effde628827252a3e82f506bdbb8826e
JungHanYoung <8735132@naver.com>
2021-05-16 01:52:07 +0900

    풀이 완료 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,44 @@
 package me.hanyoung.algorithm;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String str = in.next();
+        int result = solution(str);
+        System.out.println(result);
+    }
+
+    private static int solution(String str) {
+        Stack<Integer> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if(Character.isDigit(c)) {
+                stack.push(Character.getNumericValue(c));
+            } else {
+                int front = stack.pop();
+                int rear = stack.pop();
+                switch (c) {
+                    case '+':
+                        stack.push(rear + front);
+                        break;
+                    case '-':
+                        stack.push(rear - front);
+                        break;
+                    case '*':
+                        stack.push(rear * front);
+                        break;
+                    case '/':
+                        stack.push(rear / front);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
     }
 }
