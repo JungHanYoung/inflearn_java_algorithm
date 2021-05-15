7397bfa2c74fcf0e2ae8bcc8b4c722c667dac35d
JungHanYoung <8735132@naver.com>
2021-05-16 01:23:32 +0900

    풀이 완료 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,38 @@
 package me.hanyoung.algorithm;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String str = in.next();
+        String result = solution(str);
+        System.out.println(result);
+    }
+
+    private static String solution(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : chars) {
+            switch (c) {
+                case '(':
+                    stack.push(c);
+                    break;
+                case ')':
+                    stack.pop();
+                    break;
+                default:
+                    if(stack.size() == 0) {
+                        sb.append(c);
+                    }
+            }
+        }
+
+        return sb.toString();
     }
 }
