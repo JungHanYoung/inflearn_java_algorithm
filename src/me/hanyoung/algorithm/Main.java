7c600eab31e3bc643106b04ed52c1f0445d30064
JungHanYoung <8735132@naver.com>
2021-05-16 01:08:41 +0900

    강의 지참, 재풀이 요망(TreeSet)

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,38 @@
 package me.hanyoung.algorithm;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int result = solution(n, k, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int n, int k, int[] arr) {
+
+        TreeSet<Integer> Tset = new TreeSet<>(Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int l = j+1; l < n; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        for (int x :
+                Tset) {
+            cnt++;
+            if(cnt == k) return x;
+        }
+
+        return -1;
     }
 }
