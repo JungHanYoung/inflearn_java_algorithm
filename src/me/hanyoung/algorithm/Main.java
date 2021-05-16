0bc49fc5f83229da263b334a413e46ea812bab08
JungHanYoung <8735132@naver.com>
2021-05-16 13:48:10 +0900

    강의 지참, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,36 @@
 package me.hanyoung.algorithm;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        String arrange = in.next();
+        String study = in.next();
+        String result = solution(arrange, study);
+        System.out.println(result);
+    }
+
+    private static String solution(String arrange, String study) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c :
+                arrange.toCharArray()) {
+            queue.offer(c);
+        }
+        for (char c :
+                study.toCharArray()) {
+            if(queue.contains(c)) {
+                if(queue.peek() != c) {
+                    return "NO";
+                } else {
+                    queue.poll();
+                }
+            }
+        }
+        if(queue.isEmpty()) return "YES";
+        else return "NO";
     }
 }
