1ec53261484bedc9023eba99fca635bedc98fee5
JungHanYoung <8735132@naver.com>
2021-05-18 01:51:14 +0900

    풀이 및 채점 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        solution(n, arr);
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+
+    private static void solution(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j = i-1;
+            for (; j >= 0; j--) {
+                if(arr[j] > temp) arr[j+1]=arr[j];
+                else break;
+            }
+            arr[j+1] = temp;
+        }
     }
 }
