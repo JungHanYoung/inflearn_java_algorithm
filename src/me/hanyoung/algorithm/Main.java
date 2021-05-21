1d260a8a9a96f5a9463a00a4206040d2f4ccc704
JungHanYoung <8735132@naver.com>
2021-05-21 13:10:17 +0900

    풀이 및 채점완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,29 @@
 package me.hanyoung.algorithm;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int result = solution(n, m, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int n, int m, int[] arr) {
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == m) return i + 1;
+        }
+
+        return -1;
     }
 }
