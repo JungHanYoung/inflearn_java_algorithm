7d59f9e63cf659cbd0742e270c87236f5bbba8b2
JungHanYoung <8735132@naver.com>
2021-05-21 12:02:57 +0900

    강의 지참, 재풀이 요망

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,31 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int s = in.nextInt();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int[] result = solution(s, n, arr);
+        for (int x :
+                result) {
+            System.out.print(x + " ");
+        }
+    }
+
+    private static int[] solution(int s, int n, int[] arr) {
+        int[] cache = new int[s];
+        for (int val : arr) {
+            int pos = -1;
+            for (int i = 0; i < s; i++) {
+                if(cache[i] == val) pos = i;
+            }
+            for (int i = pos == -1 ? s-1 : pos; i >= 1; i--) {
+                cache[i] = cache[i-1];
+            }
+            cache[0] = val;
+        }
+        return cache;
     }
 }
