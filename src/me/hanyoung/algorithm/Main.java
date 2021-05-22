cc157353ccad06bf8d296ceb0cef7c940debd7fd
JungHanYoung <8735132@naver.com>
2021-05-22 13:03:10 +0900

    풀이 및 채점완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -6,8 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int result = solution(n, k, arr);
+        System.out.println(result);
+    }
+
+    private static int solution(int n, int k, int[] arr) {
+        int answer = Integer.MIN_VALUE;
+        int lt = 0;
+        int sum = 0;
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if(rt-lt >= k) {
+                sum -= arr[lt];
+                lt++;
+            }
+            if(answer < sum) answer = sum;
+        }
+        return answer;
     }
 }
