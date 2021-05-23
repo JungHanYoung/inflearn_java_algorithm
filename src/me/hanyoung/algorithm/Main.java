0245a68c6e72ba0d0b2f85daaa836609416e9dfb
JungHanYoung <8735132@naver.com>
2021-05-23 14:25:32 +0900

    풀이완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[] fibo;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        fibo = new int[n+1];
+        solution(n);
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibo[i] + " ");
+        }
+    }
+
+    private static int solution(int n) {
+        if(fibo[n] > 0) return fibo[n];
+        if(n == 1) return fibo[n] = 1;
+        if(n == 2) return fibo[n] = 1;
+        return fibo[n] = solution(n-2) + solution(n-1);
     }
 }
