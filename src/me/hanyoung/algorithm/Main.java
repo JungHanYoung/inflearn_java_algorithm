d9bab1289ea052c5dd8094f5550007f8f17c5a6f
JungHanYoung <8735132@naver.com>
2021-05-23 14:37:29 +0900

    풀이완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -2,12 +2,34 @@ package me.hanyoung.algorithm;
 
 import java.util.Scanner;
 
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+    Node(int data){
+        this.data = data;
+    }
+}
+
 public class Main {
+    Node root;
+
+    public void DFS(Node root) {
+        if(root == null) return;
+        System.out.println(root.data);
+        DFS(root.lt);
+        DFS(root.rt);
+    }
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        Main tree = new Main();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.rt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.DFS(tree.root);
     }
 }
