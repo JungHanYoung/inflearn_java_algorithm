7b2b7ec8cd9e4c44c869c43dc7f69c5c27784cee
JungHanYoung <8735132@naver.com>
2021-05-23 15:03:13 +0900

    강의지참, 복습필요

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -3,11 +3,28 @@ package me.hanyoung.algorithm;
 import java.util.Scanner;
 
 public class Main {
+    static int n;
+    static int[] ch;
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        n = in.nextInt();
+        ch = new int[n+1];
+        DFS(1);
+    }
+
+    private static void DFS(int x) {
+        if(x == n+1) {
+            StringBuilder tmp = new StringBuilder();
+            for (int i = 1; i <= n; i++) {
+                if(ch[i] > 0) tmp.append(i).append(" ");
+            }
+            if(tmp.length() > 0) System.out.println(tmp);
+            return;
+        }
+        ch[x] = 1;
+        DFS(x+1);
+        ch[x] = 0;
+        DFS(x+1);
     }
 }
