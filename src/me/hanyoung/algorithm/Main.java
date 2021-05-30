790f09f4279a3b3dab5026da8808a9840fce90d5
JungHanYoung <8735132@naver.com>
2021-05-30 22:02:59 +0900

    챕터연습

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,48 @@
 package me.hanyoung.algorithm;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+    public Node(int data) {
+        this.data = data;
+    }
+}
 
 public class Main {
 
+    Node root;
+
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        Main tree = new Main();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(BFS(tree.root));
+    }
+
+    private static int BFS(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        int level = 0;
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i <size; i++) {
+                Node cur = q.poll();
+                if(cur.lt==null && cur.rt==null) {
+                    return level;
+                }
+                if(cur.lt!=null) q.offer(cur.lt);
+                if(cur.rt!=null) q.offer(cur.rt);
+            }
+            level++;
+        }
+        return level;
     }
 }
