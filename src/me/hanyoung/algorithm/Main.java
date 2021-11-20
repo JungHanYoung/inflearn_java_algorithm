c753c2094c60d108d10d1550667f6df66381deeb
JungHanYoung <8735132@naver.com>
2021-11-21 01:08:09 +0900

    문제풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,43 @@
 package me.hanyoung.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static int n, m, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        n = in.nextInt();
+        m = in.nextInt();
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        ch = new int[n+1];
+        for (int i = 0; i < m; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1]=1;
+        DFS(1);
+        System.out.println(answer);
+    }
+
+    private static void DFS(int v) {
+        if(v == n) answer++;
+        else {
+            for (int nv : graph.get(v)) {
+                if(ch[nv]==0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
+                }
+            }
+        }
     }
 }
