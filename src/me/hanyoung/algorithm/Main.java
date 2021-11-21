8c768dad382c2a2721a389ae44298b5eccecfb44
JungHanYoung <8735132@naver.com>
2021-11-21 18:06:03 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[] arr;
+    static int c;
+    static int max = Integer.MIN_VALUE;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        c = in.nextInt();
+        int count = in.nextInt();
+        arr = new int[count];
+        for (int i = 0; i < count; i++) {
+            arr[i] = in.nextInt();
+        }
+        DFS(0, 0);
+        System.out.println(max);
+    }
+
+    private static void DFS(int idx, int sum) {
+        if(sum <= c) {
+            if(sum > max) {
+                max = sum;
+            }
+            if(idx == arr.length) {
+                return;
+            }
+            DFS(idx+1, sum + arr[idx]);
+            DFS(idx+1, sum);
+        }
     }
 }
