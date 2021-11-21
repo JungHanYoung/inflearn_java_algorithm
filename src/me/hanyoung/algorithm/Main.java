68161057db0982fe054062c81add08fbdfe30e5e
JungHanYoung <8735132@naver.com>
2021-11-21 20:29:56 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,34 @@
 package me.hanyoung.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
+    static int[] arr;
+    static int N, M;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        N = in.nextInt();
+        M = in.nextInt();
+        BFS(new Stack<>());
+    }
+
+    private static void BFS(Stack<Integer> list) {
+        if(list.size() == M) {
+            for(int i : list) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        } else {
+            for(int i = 1; i <= N; i++) {
+                list.push(i);
+                BFS(list);
+                list.pop();
+            }
+        }
     }
 }
