5e439226a480c0eddc3ff81b1a8071962898735c
JungHanYoung <8735132@naver.com>
2021-11-21 20:16:10 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,32 @@
 package me.hanyoung.algorithm;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
+    static int limit, max = Integer.MIN_VALUE;
+    static int[] scores, times;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        limit = in.nextInt();
+        scores = new int[n+1];
+        times = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            scores[i] = in.nextInt();
+            times[i] = in.nextInt();
+        }
+        DFS(1, 0, 0);
+        System.out.println(max);
+    }
+
+    private static void DFS(int idx, int sum, int time) {
+        if(time > limit) return;
+        if(max < sum) max = sum;
+        if(idx == scores.length) return;
+        DFS(idx+1, sum+scores[idx], time+times[idx]);
+        DFS(idx+1, sum, time);
     }
 }
