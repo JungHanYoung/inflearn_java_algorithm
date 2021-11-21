2d3f61c30a4e734527a5a2b0e078f0e3921fa320
JungHanYoung <8735132@naver.com>
2021-11-21 15:19:09 +0900

    7-14 그래프 최단거리 문제풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,47 @@
 package me.hanyoung.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    private static int n, m;
+    private static List<List<Integer>> h = new ArrayList<>();
+    private static int[] arr;
+    private static boolean[] checks;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        n = in.nextInt();
+        m = in.nextInt();
+        arr = new int[n+1];
+        checks = new boolean[n+1];
+        for(int i = 0; i <= n; i++) {
+            h.add(new ArrayList<>());
+            arr[i] = Integer.MAX_VALUE;
+        }
+        for(int i = 0; i < m; i++) {
+            int startPoint = in.nextInt();
+            int endPoint = in.nextInt();
+            h.get(startPoint).add(endPoint);
+        }
+        checks[1] = true;
+        BFS(1, 0);
+        for(int i = 2; i <= n; i++) {
+            System.out.printf("%d : %d%n", i, arr[i]);
+        }
+    }
+
+    private static void BFS(int point, int distance) {
+        if(arr[point] > distance) arr[point] = distance;
+        for(int p : h.get(point)) {
+            if(!checks[p]) {
+                checks[p] = true;
+                BFS(p, distance+1);
+                checks[p] = false;
+            }
+        }
     }
 }
