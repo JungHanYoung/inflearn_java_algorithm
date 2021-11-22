df91e92ae001e86e18cd007f4b07f6cc7b2a063e
JungHanYoung <8735132@naver.com>
2021-11-22 12:33:16 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,38 @@
 package me.hanyoung.algorithm;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
+    static int M;
+    static int[] arr;
+    static Stack<Integer> stack = new Stack<>();
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int N = in.nextInt();
+        M = in.nextInt();
+        arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+        BFS(0);
+    }
+
+    private static void BFS(int level) {
+        if(level == M) {
+            for (int i: stack) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        } else {
+            for (int j : arr) {
+                if (stack.contains(j)) continue;
+                stack.push(j);
+                BFS(level + 1);
+                stack.pop();
+            }
+        }
     }
 }
