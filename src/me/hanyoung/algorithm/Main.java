0c2b03234338d7f463863dc7fcd7b78d10ee04aa
JungHanYoung <8735132@naver.com>
2021-11-23 15:52:37 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,30 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[][] arr;
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int n = in.nextInt();
+        int r = in.nextInt();
+        arr = new int[n+1][r+1];
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= r; j++) {
+                arr[i][j] = -1;
+            }
+        }
+        int result = DP(n, r);
+        System.out.println(result);
+    }
+
+    private static int DP(int n, int r) {
+        if(n == r || r == 0) {
+            return 1;
+        } else if(arr[n][r] > 0) {
+            return arr[n][r];
+        } else {
+            arr[n-1][r-1] = DP(n-1, r-1);
+            arr[n-1][r] = DP(n-1, r);
+            return arr[n-1][r-1] + arr[n-1][r];
+        }
     }
 }
