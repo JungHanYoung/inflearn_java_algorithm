2be56742cad349cd1764b454196f5de4fcd2f204
JungHanYoung <8735132@naver.com>
2021-11-23 18:05:27 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[] arr;
+    static int N, M;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        N = in.nextInt();
+        M = in.nextInt();
+        arr = new int[M];
+        DFS(0, 1);
+    }
+
+    private static void DFS(int l, int s) {
+        if(l == M) {
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for(int i = s; i <= N; i++) {
+                arr[l] = i;
+                DFS(l+1, i+1);
+            }
+        }
     }
 }
