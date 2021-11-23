d51e413f18bd13ffa5c0fca2c3d319358e2b9510
JungHanYoung <8735132@naver.com>
2021-11-23 17:45:59 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -4,10 +4,47 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int F, N;
+    static boolean flag = false;
+    static int[][] dp = new int[11][11];
+    static int[] arr, p, checks;
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        N = in.nextInt();
+        F = in.nextInt();
+        arr = new int[N];
+        p = new int[N];
+        checks = new int[N+1];
+        for(int i = 0; i < N; i++) {
+            arr[i] = comb(N-1, i);
+        }
+
+        DFS(0, 0);
+    }
+
+    private static void DFS(int l, int sum) {
+        if(flag) return;
+        if(l == N) {
+            if(sum == F) {
+                for (int i : p) System.out.print(i + " ");
+                flag = true;
+            }
+        } else {
+            for(int i = 1; i <= N; i++) {
+                if(checks[i] == 0) {
+                    checks[i] = 1;
+                    p[l] = i;
+                    DFS(l+1, sum + (p[l] * arr[l]));
+                    checks[i] = 0;
+                }
+            }
+        }
+    }
+
+    private static int comb(int n, int r) {
+        if(dp[n][r] > 0) return dp[n][r];
+        if(n == r || r == 0) return 1;
+        else return dp[n][r] = comb(n-1, r-1) + comb(n-1, r);
     }
 }
