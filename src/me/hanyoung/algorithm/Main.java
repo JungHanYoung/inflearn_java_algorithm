362e32e79699b477ad2cee82e21c5988c29d3415
JungHanYoung <8735132@naver.com>
2021-11-25 23:40:36 +0900

    문제 풀이 완료

--- src/me/hanyoung/algorithm/Main.java
+++ src/me/hanyoung/algorithm/Main.java
@@ -1,13 +1,43 @@
 package me.hanyoung.algorithm;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
+        int N = in.nextInt();
+        SsirmSunsu[] arr = new SsirmSunsu[N];
+        for (int i = 0; i < N; i++) {
+            int height = in.nextInt();
+            int weight = in.nextInt();
+            arr[i] = new SsirmSunsu(height, weight);
+        }
+        List<SsirmSunsu> sunsuList = Arrays
+                .stream(arr)
+                .sorted((sunsu1, sunsu2) -> sunsu2.height - sunsu1.height)
+                .collect(Collectors.toList());
+        int cnt = 0;
+        int max = Integer.MIN_VALUE;
+        for (SsirmSunsu ssirmSunsu : sunsuList) {
+            int weight = ssirmSunsu.weight;
+            if (weight > max) {
+                cnt++;
+                max = weight;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    private static class SsirmSunsu {
+        int height;
+        int weight;
+        SsirmSunsu(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
     }
 }
